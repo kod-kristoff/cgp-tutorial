f06b7724f4655bc13e7fed79b85a75dee1ade95c
Kristoffer Andersson <kod.kristoff@gmail.com>
2025-05-19 16:56:49 +0200

    greeter level 1

--- examples/greeter/src/main.rs
+++ examples/greeter/src/main.rs
@@ -1,19 +1,25 @@
-struct Person {
-    name: String,
+trait HasName {
+    fn name(&self) -> &str;
+}
+fn greet<Context>(context: &Context)
+where
+    Context: HasName,
+{
+    println!("Hello, {}!", context.name());
 }
 
-impl Person {
-    fn name(&self) -> &str {
-        &self.name
-    }
-}
-
-fn greet(person: &Person) {
-    println!("Hello, {}!", person.name());
-}
 fn main() {
     let steve = Person {
         name: "Steve".to_string(),
     };
     greet(&steve);
+}
+struct Person {
+    name: String,
+}
+
+impl HasName for Person {
+    fn name(&self) -> &str {
+        &self.name
+    }
 }
