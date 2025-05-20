3e98bfa48dee8a8ee432be37fa928e03cc40ee4b
Kristoffer Andersson <kod.kristoff@gmail.com>
2025-05-20 19:04:00 +0200

    string-formatter: add json

--- examples/string-formatter/src/main.rs
+++ examples/string-formatter/src/main.rs
@@ -1,18 +1,22 @@
 use std::fmt::{self, Display};
 
-use impls::{FormatStringWithDebug, FormatStringWithDisplay};
+use anyhow::Error;
+use impls::{FormatAsJson, FormatStringWithDebug, FormatStringWithDisplay};
 
 /// Consumer
 pub trait CanFormatString {
-    fn format_string(&self) -> String;
+    fn format_string(&self) -> Result<String, Error>;
 }
 /// Provider
 pub trait StringFormatter<Context> {
-    fn format_string(context: &Context) -> String;
+    fn format_string(context: &Context) -> Result<String, Error>;
 }
 /// impls
 mod impls {
     use std::fmt::{Debug, Display};
+
+    use anyhow::Error;
+    use serde::Serialize;
 
     use crate::StringFormatter;
 
@@ -22,8 +26,8 @@ mod impls {
     where
         Context: Display,
     {
-        fn format_string(context: &Context) -> String {
-            format!("{}", context)
+        fn format_string(context: &Context) -> Result<String, Error> {
+            Ok(format!("{}", context))
         }
     }
 
@@ -31,8 +35,19 @@ mod impls {
     where
         Context: Debug,
     {
-        fn format_string(context: &Context) -> String {
-            format!("{:?}", context)
+        fn format_string(context: &Context) -> Result<String, Error> {
+            Ok(format!("{:?}", context))
+        }
+    }
+
+    pub struct FormatAsJson;
+
+    impl<Context> StringFormatter<Context> for FormatAsJson
+    where
+        Context: Serialize,
+    {
+        fn format_string(context: &Context) -> Result<String, Error> {
+            Ok(serde_json::to_string(context)?)
         }
     }
 }
@@ -41,11 +56,15 @@ fn main() {
         first_name: "John".into(),
         last_name: "Smith".into(),
     };
-    println!("{}", FormatStringWithDisplay::format_string(&person));
-    println!("{}", FormatStringWithDebug::format_string(&person));
+    println!(
+        "{}",
+        FormatStringWithDisplay::format_string(&person).unwrap()
+    );
+    println!("{}", FormatStringWithDebug::format_string(&person).unwrap());
+    println!("{}", FormatAsJson::format_string(&person).unwrap());
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct Person {
     first_name: String,
     last_name: String,
