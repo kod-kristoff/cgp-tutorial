0ca22f5d8d579f61bf22b27e4dbe8a6af4fac33b
Kristoffer Andersson <kod.kristoff@gmail.com>
2025-05-20 20:12:18 +0200

    string-formatter: add ParseFromString

--- examples/string-formatter/src/main.rs
+++ examples/string-formatter/src/main.rs
@@ -1,66 +1,73 @@
 use std::fmt::{self, Display};
 
 use anyhow::Error;
-use impls::{FormatAsJson, FormatStringWithDebug, FormatStringWithDisplay};
+use impls::{FormatAsJsonString, ParseFromJsonString};
 
 pub trait HasCgpProvider {
     type CgpProvider;
 }
-/// Consumer
-pub trait CanFormatString {
-    fn format_string(&self) -> Result<String, Error>;
+/// Consumers
+pub trait CanFormatToString {
+    fn format_to_string(&self) -> Result<String, Error>;
 }
 
-impl<Context> CanFormatString for Context
+pub trait CanParseFromString: Sized {
+    fn parse_from_string(raw: &str) -> Result<Self, Error>;
+}
+
+/// Provider
+pub trait StringFormatter<Context> {
+    fn format_to_string(context: &Context) -> Result<String, Error>;
+}
+pub trait StringParser<Context> {
+    fn parse_from_string(raw: &str) -> Result<Context, Error>;
+}
+
+impl<Context> CanFormatToString for Context
 where
     Context: HasCgpProvider,
     Context::CgpProvider: StringFormatter<Context>,
 {
-    fn format_string(&self) -> Result<String, Error> {
-        Context::CgpProvider::format_string(self)
+    fn format_to_string(&self) -> Result<String, Error> {
+        Context::CgpProvider::format_to_string(self)
     }
 }
-/// Provider
-pub trait StringFormatter<Context> {
-    fn format_string(context: &Context) -> Result<String, Error>;
+impl<Context> CanParseFromString for Context
+where
+    Context: HasCgpProvider,
+    Context::CgpProvider: StringParser<Context>,
+{
+    fn parse_from_string(raw: &str) -> Result<Context, Error> {
+        Context::CgpProvider::parse_from_string(raw)
+    }
 }
 /// impls
 mod impls {
-    use std::fmt::{Debug, Display};
 
     use anyhow::Error;
-    use serde::Serialize;
+    use serde::{Deserialize, Serialize};
 
-    use crate::StringFormatter;
+    use crate::{StringFormatter, StringParser};
 
-    pub struct FormatStringWithDisplay;
-    pub struct FormatStringWithDebug;
-    impl<Context> StringFormatter<Context> for FormatStringWithDisplay
-    where
-        Context: Display,
-    {
-        fn format_string(context: &Context) -> Result<String, Error> {
-            Ok(format!("{}", context))
-        }
-    }
+    pub struct FormatAsJsonString;
 
-    impl<Context> StringFormatter<Context> for FormatStringWithDebug
-    where
-        Context: Debug,
-    {
-        fn format_string(context: &Context) -> Result<String, Error> {
-            Ok(format!("{:?}", context))
-        }
-    }
-
-    pub struct FormatAsJson;
-
-    impl<Context> StringFormatter<Context> for FormatAsJson
+    impl<Context> StringFormatter<Context> for FormatAsJsonString
     where
         Context: Serialize,
     {
-        fn format_string(context: &Context) -> Result<String, Error> {
+        fn format_to_string(context: &Context) -> Result<String, Error> {
             Ok(serde_json::to_string(context)?)
+        }
+    }
+
+    pub struct ParseFromJsonString;
+
+    impl<Context> StringParser<Context> for ParseFromJsonString
+    where
+        Context: for<'a> Deserialize<'a>,
+    {
+        fn parse_from_string(json_str: &str) -> Result<Context, Error> {
+            Ok(serde_json::from_str(json_str)?)
         }
     }
 }
@@ -69,16 +76,13 @@ fn main() {
         first_name: "John".into(),
         last_name: "Smith".into(),
     };
-    println!(
-        "{}",
-        FormatStringWithDisplay::format_string(&person).unwrap()
-    );
-    println!("{}", FormatStringWithDebug::format_string(&person).unwrap());
-    println!("{}", FormatAsJson::format_string(&person).unwrap());
-    println!("{}", person.format_string().unwrap());
+    let person_str = r#"{"first_name":"John","last_name":"Smith"}"#;
+    assert_eq!(person.format_to_string().unwrap(), person_str);
+    assert_eq!(Person::parse_from_string(person_str).unwrap(), person);
+    println!("{}", person.format_to_string().unwrap());
 }
 
-#[derive(Debug, serde::Serialize)]
+#[derive(Debug, serde::Serialize, serde::Deserialize, PartialEq)]
 struct Person {
     first_name: String,
     last_name: String,
@@ -89,7 +93,20 @@ impl Display for Person {
         write!(f, "{} {}", self.first_name, self.last_name)
     }
 }
+pub struct PersonComponents;
 
 impl HasCgpProvider for Person {
-    type CgpProvider = FormatStringWithDisplay;
+    type CgpProvider = PersonComponents;
+}
+
+impl StringFormatter<Person> for PersonComponents {
+    fn format_to_string(person: &Person) -> Result<String, Error> {
+        FormatAsJsonString::format_to_string(person)
+    }
+}
+
+impl StringParser<Person> for PersonComponents {
+    fn parse_from_string(raw: &str) -> Result<Person, Error> {
+        ParseFromJsonString::parse_from_string(raw)
+    }
 }
